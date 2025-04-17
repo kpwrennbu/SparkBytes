a80f5f7a45f089ffd78e2c44182689472d13b56a
kpwrennbu <kpwrenn@bu.edu>
2025-04-17 18:11:07 -0400

    minor styling to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,42 +21,18 @@ const styles = {
     margin: "0 auto",
     padding: "32px 16px",
     fontFamily: "Segoe UI, sans-serif",
+    display: 'flex', 
+    justifyContent: "space-around", 
+    alignItems: "start",
+    gap: "5em"
   },
-  header: {
-    display: "flex",
-    flexDirection: "column" as const,
-    alignItems: "center",
-    marginBottom: "32px",
-  },
-  controlBar: {
-    display: "flex",
-    flexWrap: "wrap" as const,
-    justifyContent: "space-between",
-    gap: "16px",
-    marginBottom: "24px",
-    padding: "0 12px",
-  },
-  sortControls: {
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-    flexWrap: "wrap" as const,
-  },
-  searchInput: {
-    padding: "10px",
-    borderRadius: "8px",
-    fontSize: "16px",
-    width: "300px",
-    flex: "1 0 250px",
-  },
-
-
 };
 
 
 
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<{ lat: number; lng: number } | null>(null);
 
   useEffect(() => {
     if (navigator.geolocation) {
